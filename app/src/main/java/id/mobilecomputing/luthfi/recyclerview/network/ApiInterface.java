cc20dc7e2cfe5421ec34d6c86f2259c3b5234b90
unknown <luthfiaryarizki25@gmail.com>
2020-08-18 09:14:54 +0700

    Recycler View

---
+++ app/src/main/java/id/mobilecomputing/luthfi/recyclerview/network/ApiInterface.java
@@ -0,0 +1,12 @@
+package id.mobilecomputing.luthfi.recyclerview.network;
+
+import id.mobilecomputing.luthfi.recyclerview.model.MoviesPopularResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("popular")
+    Call<MoviesPopularResponse> getMoviePopular(@Query("api_key") String api_key);
+
+}
